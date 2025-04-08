457fb9e8375559c2c76dc7e26ed8eec0bcd870b8
Ramyashri14-0593 <ramtekeramyashri@gmail.com>
2025-04-08 18:19:52 +0530

    Add files via upload

---
+++ Spectral_features_find.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  8 01:19:42 2025
+
+@author: Ramyashri Ramteke
+"""
+
+
+import numpy as np
+from scipy.signal import hilbert
+
+def compute_instfreq_batch(data, fs):
+    """
+    Compute instantaneous frequency for a batch of signals.
+
+    Parameters:
+        data: 2D array (n_signals, n_samples)
+        fs: Sampling frequency (Hz)
+
+    Returns:
+        inst_freqs: List of 1D arrays (one per signal)
+        t: Time vector (shared for all)
+    """
+import numpy as np
+from scipy.signal import spectrogram
+from scipy.stats import entropy
+
+def compute_instfreq_and_entropy(data, fs=360, n_windows=524):
+    """
+    data: ndarray shape (N_signals, N_samples)
+    fs: sampling rate (Hz)
+    n_windows: number of time windows for spectrogram
+    """
+    N_signals, N_samples = data.shape
+    instfreq_all = []
+    pentropy_all = []
+
+    # Calculate nperseg from desired number of time windows
+    noverlap = 0
+    nperseg = int(N_samples / n_windows)
+
+    for i in range(N_signals):
+        signal = data[i]
+
+        # Compute spectrogram
+        f, t, Sxx = spectrogram(signal, fs=fs, nperseg=nperseg, noverlap=noverlap, mode='psd')
+        Sxx = np.maximum(Sxx, 1e-12)  # Avoid log(0)
+
+        # Normalize the spectrum over frequencies to get a distribution
+        Sxx_norm = Sxx / np.sum(Sxx, axis=0, keepdims=True)
+
+        # 1. Instantaneous Frequency (first moment of power spectrum)
+        inst_freq = np.sum(f[:, np.newaxis] * Sxx_norm, axis=0)
+
+        # 2. Spectral Entropy
+        spec_entropy = entropy(Sxx_norm, base=2, axis=0)
+
+        instfreq_all.append(inst_freq)
+        pentropy_all.append(spec_entropy)
+
+    return np.array(instfreq_all), np.array(pentropy_all), t
+
+
+
+loaded = np.load("ECG_samples.npy")
+instfreqs, pentropies, times = compute_instfreq_and_entropy(loaded, fs=360)
+
+print("instfreqs shape:", instfreqs.shape)  # (16, 524)
+print("pentropies shape:", pentropies.shape)
+
+# Stack along the last axis (feature axis)
+combined_features = np.stack((instfreqs, pentropies), axis=2)
+
+print("Combined shape:", combined_features.shape)  # (16, 524, 2)
+
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.plot(times, instfreqs[0])
+plt.title("Instantaneous Frequency")
+plt.xlabel("Time (s)")
+plt.ylabel("Frequency (Hz)")
+
+plt.subplot(1, 2, 2)
+plt.plot(times, pentropies[0])
+plt.title("Spectral Entropy")
+plt.xlabel("Time (s)")
+plt.ylabel("Entropy (bits)")
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
